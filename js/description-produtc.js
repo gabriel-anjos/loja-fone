e13b3aa1f77d667ce76068ee2c21ab702f631c51
Gabriel Anjos <anjos.gabriel999@gmail.com>
2021-11-16 12:06:40 -0300

    function star

--- js/description-produtc.js
+++ js/description-produtc.js
@@ -4,8 +4,26 @@ const $positionLast = $Stars.length -1;
 
 $heart.addEventListener('click', handleClick);
 
-$Stars[0].addEventListener('click', handleClick );
+// $Stars[0].addEventListener('click', handleClick );
 $Stars[$positionLast].addEventListener('click', lastStar);
+
+
+$Stars.forEach(function($star, key){
+    if(key == 0){
+        $star.addEventListener('click', firstStar)
+    }
+    if(key == $positionLast){
+        $star.addEventListener('click',lastStar)
+    }
+    if(key > 0 && key < $positionLast){
+        $star.addEventListener('click',function(){
+            middleStar(key)
+        })
+    }
+})
+
+
+
 
 // $Stars.forEach(function($Star){
 //     $Star.addEventListener('click',handleClick)
@@ -29,6 +47,24 @@ function handleClick(){
 // function handleStar(){
 //     this.classList.toggle('-active');
 // }
+
+function firstStar(){
+    $Stars.forEach(function($star){
+        $star.classList.remove('-active')
+    });
+    this.classList.add('-active');
+}
+
+function middleStar(index){
+    $Stars.forEach(function($star ,key){
+        $star.classList.remove('-active');
+
+        if(key <= index){
+            $star.classList.add('-active');
+        }
+    });
+    
+}
 
 function lastStar(){
     $Stars.forEach(function($star){
