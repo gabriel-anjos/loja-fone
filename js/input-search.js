b27a6cae76f908d429171602873a1307eb11a0cc
Gabriel Anjos <anjos.gabriel999@gmail.com>
2021-11-17 13:08:07 -0300

    open and close function search

--- js/input-search.js
+++ js/input-search.js
@@ -1,9 +1,18 @@
 const $inputSearch = document.querySelector('.input-search');
-const $icon = $inputSearch.querySelector('.icon')
-$icon.addEventListener('click', event =>{
+const $open = $inputSearch.querySelector('.-open');
+const $close = $inputSearch.querySelector('.-close');
+$open.addEventListener('click', event =>{
 
         event.preventDefault();
 
         $inputSearch.classList.toggle('-active');
+        $open.classList.remove('-active');
+        $close.classList.add('-active');
     });
-;
+
+$close.addEventListener('click', event =>{
+    event.preventDefault();
+    $inputSearch.classList.toggle('-active');
+    $close.classList.remove('-active');
+    $open.classList.add('-active');
+})
